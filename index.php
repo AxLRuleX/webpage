816e3621c41852b3d86424005e075469cebfc26a
AxLRuleX <38544991+AxLRuleX@users.noreply.github.com>
2018-07-10 00:22:16 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>AxL C.A</title>
+<link href="Css/estilo.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Dosis:500" rel="stylesheet">
+</head>
+	<header>
+		<div id="cab">
+			<div id="box_titulo">
+				<p id="titulo"> AxL. C.A</p>
+			</div>
+			<div id="social_media">
+				<img class="logo" src="Imagenes/logo_facebook.png">
+				<img class="logo" src="Imagenes/logo_youtube.png">
+			</div>
+		</div>
+	</header>
+<body>
+
+	<div id="cuerpo">
+		<div id="columna1">
+			<div id="cuadro1" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud1" class="flipper">
+					<div class="front1">
+						<p id="text-c1">¿Quienes<br> somos?</p>
+					</div>
+					<div class="back1">
+						Somos una empresa<br>
+						dedicada al desarrollo de<br>
+						soluciones informáticas<br>
+						<a href="#"><p>Seguir leyendo...</p></a>
+					</div> 
+				</div>
+			</div>
+
+			<div id="cuadro3" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud3" class="flipper">
+					<div class="front3">
+						<p>Otro Cuadro</p>
+					</div>
+					<div class="back3">
+						Somos una empresa<br>
+						dedicada al desarrollo de<br>
+						soluciones informáticas<br>
+						<a href="#"><p>Seguir leyendo...</p></a>
+					</div> 
+				</div>
+			</div>
+
+			<div id="cuadro5" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud5" class="flipper">
+					<div class="front5">
+						<p>Otro Cuadro</p>
+					</div>
+					<div class="back5">
+						Somos una empresa<br>
+						dedicada al desarrollo de<br>
+						soluciones informáticas<br>
+						<a href="#"><p>Seguir leyendo...</p></a>
+					</div> 
+				</div>
+			</div>
+		</div>
+
+		<div id="columna2">
+			<div  id="cuadro2" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud2" class="flipper">
+					<div class="front2">
+						<div>
+							
+						</div>
+					</div>
+					<div class="back2">
+						Detras
+					</div> 
+				</div>
+			</div>
+
+			<div  id="cuadro6" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud6" class="flipper">
+					<div class="front6">
+						<div>
+							
+						</div>
+					</div>
+					<div class="back6">
+						Detras
+					</div> 
+				</div>
+			</div>
+		</div>
+
+		<div id="columna3">
+			<div  id="cuadro4" class="flip-container" ontouchstart="this.classlist.toggle('hover')">
+				<div id="cud4" class="flipper">
+					<div class="front4">
+						<div>
+							
+						</div>
+					</div>
+					<div class="back4">
+						Detras
+					</div> 
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<footer>
+	<div id="pie">
+			<div id="box_pie">
+				<p id="texto-pie"> AxL. C.A 2018 ®</p>
+			</div>
+	</div>
+</footer>
+</html>
